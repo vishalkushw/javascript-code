5cc6ecd9478ba481a4f48daa92e09bb5796ebcde
Vishal kushwah <110225964+vishalkushw@users.noreply.github.com>
2024-10-08 11:04:48 +0530

    Aug08 variable

---
+++ javascript class code Agust/Aug08  var/variable.js
@@ -0,0 +1,9 @@
+var name="vishal";
+var a=67;
+var b =true;
+
+document.write(name +" : "+typeof(name) );
+document.write("<br>");
+document.write(a +" : "+typeof(a) );
+document.write("<br>");
+document.write( b+" : "+typeof(b) );

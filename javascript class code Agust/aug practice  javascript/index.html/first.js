fe4560575d001088d85133f40fda87a783e3ff36
Vishal kushwah <110225964+vishalkushw@users.noreply.github.com>
2024-10-09 14:15:50 +0530

    Aug practice javascript

---
+++ javascript class code Agust/aug practice  javascript/index.html/first.js
@@ -0,0 +1,15 @@
+var age=20;
+var b=30;
+var nam="vishal";
+
+document.write("<h4>"+"Hi my name  is " +nam+" "+" and my age is "+age);
+
+
+function display(){
+
+      mycollage=nm=>"my collage name is "+nm;
+
+        document.getElementById("demo").innerHTML=mycollage("sam");
+     
+
+}

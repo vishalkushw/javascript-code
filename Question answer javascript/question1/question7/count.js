eea88e014792faf61fdd13ba294a12d5026a6dfe
Vishal kushwah <110225964+vishalkushw@users.noreply.github.com>
2024-10-10 11:19:33 +0530

    question7 minimum

---
+++ Question answer javascript/question1/question7/count.js
@@ -0,0 +1,21 @@
+var count = 0;
+        var myInt;
+
+        function myFun() {
+            document.getElementById("demo").innerHTML = "Happy Birthday, bro!";
+            clearInterval(myInt); 
+        }
+
+        myInt = setInterval(function() {
+            count++;
+            document.getElementById("counter").innerHTML = count;
+
+            if (count >= 5) {
+                myFun(); 
+            }
+        }, 1000); 
+
+        function myStopnow() {
+            clearInterval(myInt); 
+        }
+       
